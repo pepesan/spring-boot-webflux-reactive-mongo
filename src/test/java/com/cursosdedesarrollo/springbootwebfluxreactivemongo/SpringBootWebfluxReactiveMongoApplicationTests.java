ce77ac2d665caf0f0180ac9839ff10d02c56df6b
pepesan <pepesan@gmailcom>
2019-05-27 13:33:47 +0200

    ejemplo de prueba hecha con el webclienttest

--- src/test/java/com/cursosdedesarrollo/springbootwebfluxreactivemongo/SpringBootWebfluxReactiveMongoApplicationTests.java
+++ src/test/java/com/cursosdedesarrollo/springbootwebfluxreactivemongo/SpringBootWebfluxReactiveMongoApplicationTests.java
@@ -1,16 +1,39 @@
 package com.cursosdedesarrollo.springbootwebfluxreactivemongo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClientConfigurer;
+
+import java.time.Duration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxReactiveMongoApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    private WebTestClient webClient;
+    @Before
+    public void setUp() {
+        this.webClient= WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:8080")
+                .build();
     }
 
+
+    @Test
+    public void testObtenerListaPersonas() {
+        byte[] resultado = webClient
+                .get().uri("/api/persons")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().returnResult().getResponseBody();
+        System.out.format("%s: %s%n", "La lista de personas:",  new String(resultado));
+    }
 }
