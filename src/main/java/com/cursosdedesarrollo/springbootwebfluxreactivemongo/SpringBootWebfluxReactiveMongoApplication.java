106a54158c1ef25065c35c1055eab79faec922ac
pepesan <pepesan@gmailcom>
2019-05-27 13:10:03 +0200

    manejo de sse en spring boot con webflux

--- src/main/java/com/cursosdedesarrollo/springbootwebfluxreactivemongo/SpringBootWebfluxReactiveMongoApplication.java
+++ src/main/java/com/cursosdedesarrollo/springbootwebfluxreactivemongo/SpringBootWebfluxReactiveMongoApplication.java
@@ -1,12 +1,42 @@
 package com.cursosdedesarrollo.springbootwebfluxreactivemongo;
 
+import com.cursosdedesarrollo.springbootwebfluxreactivemongo.controllers.TemperatureController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class SpringBootWebfluxReactiveMongoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxReactiveMongoApplication.class, args);
+    }
+
+    // Cliente
+
+    Logger logger = LoggerFactory.getLogger(TemperatureController.class);
+    @Bean
+    WebClient getWebClient() {
+        return WebClient.create("http://localhost:8080");
+    }
+    @Bean
+    CommandLineRunner demo(WebClient client) {
+        return args -> {
+            client.get()
+                    .uri("/temperatures")
+                    .accept(MediaType.TEXT_EVENT_STREAM)
+                    .retrieve()
+                    .bodyToFlux(Integer.class)
+                    .map(s -> String.valueOf(s))
+                    .subscribe(msg -> {
+                        logger.info(msg);
+                    });
+        };
     }
 }
